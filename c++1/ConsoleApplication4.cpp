334925cb6e400a623dcc79aa9ef7efc2014b14dd
FixsRise <56846220+FixsRise@users.noreply.github.com>
2020-10-08 18:44:28 +0300

    Add files via upload

---
+++ c++1/ConsoleApplication4.cpp
@@ -0,0 +1,50 @@
+﻿// ConsoleApplication4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int n, n1, n2, n3, n4, n5;
+
+    setlocale(LC_ALL, "Russian");
+    
+    cout << "Введите пятизначное число : ";
+
+    cin >> n;
+
+    n1 = n/10000;
+    n2 = (n % 10000) / 1000;
+    n3 = (n % 1000) / 100;
+    n4 = (n % 100) / 10;
+    n5 = n % 10;
+
+    if (n > 9999 && n < 100000) {
+
+
+        cout << "1 цифра = " << n1;
+        cout << endl;
+        cout << "2 цифра = " << n2;
+        cout << endl;
+        cout << "3 цифра = " << n3;
+        cout << endl;
+        cout << "4 цифра = " << n4;
+        cout << endl;
+        cout << "5 цифра = " << n5;
+        cout << endl;
+
+    }
+    else {
+     
+        cout << "Вы ввели не пятизначное число";
+        cout << endl;
+
+    }
+
+
+
+
+    system("pause");
+}
