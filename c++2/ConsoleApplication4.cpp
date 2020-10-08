6cf997979654f64d71ad87c3e6444d480dcf99d5
FixsRise <56846220+FixsRise@users.noreply.github.com>
2020-10-08 19:48:20 +0300

    Add files via upload

---
+++ c++2/ConsoleApplication4.cpp
@@ -0,0 +1,30 @@
+﻿// ConsoleApplication4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    setlocale(LC_ALL, "Russian");
+
+    float a, b;
+
+    cout << "\tПеревод числа из метров в километры" << endl << endl;
+
+    cout << "Введите количество метров: ";
+    
+    cin >> a;
+
+    b = a / 1000;
+
+    cout << a << "м" << " = " << b << "км" << endl << endl;
+
+
+
+
+
+    system("pause");
+}
