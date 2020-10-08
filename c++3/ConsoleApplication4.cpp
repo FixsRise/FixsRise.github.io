4c023124735798c7c37c7f0599a982e899bf940d
FixsRise <56846220+FixsRise@users.noreply.github.com>
2020-10-08 20:02:10 +0300

    Add files via upload

---
+++ c++3/ConsoleApplication4.cpp
@@ -0,0 +1,28 @@
+﻿// ConsoleApplication4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int a[2][9] = {
+             {201,203,203,203,203,203,203,203,187},
+         {200,202,202,202,202,202,202,202,188 }
+    };
+
+    for (int i = 0; i < 2; i++) {
+
+        for (int j = 0; j < 9; j++)
+
+            cout << static_cast<char>(a[i][j]);
+        cout << endl;
+
+    }
+    
+    return 0;
+
+
+    system("pause");
+}
